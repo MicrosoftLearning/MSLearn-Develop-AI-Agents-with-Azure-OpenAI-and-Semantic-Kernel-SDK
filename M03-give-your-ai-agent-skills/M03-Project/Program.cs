c9994caead9e0d62f03b74201232d8ad3ef98dd0
Becky Zahid <buzahid@microsoft.com>
2024-02-22 16:52:33 -0800

    Added starter code & starting packages for exercises

---
+++ M03-give-your-ai-agent-skills/M03-Project/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.SemanticKernel;
+
+string yourDeploymentName = "";
+string yourEndpoint = "";
+string yourKey = "";
+
+var builder = Kernel.CreateBuilder();
+builder.Services.AddAzureOpenAIChatCompletion(
+    yourDeploymentName,
+    yourEndpoint,
+    yourKey,
+    "gpt-35-turbo-16k");
+
+var kernel = builder.Build();
+var result = await kernel.InvokePromptAsync(
+    "Give me a list of breakfast foods with eggs and cheese");
+    
+Console.WriteLine(result);
---
+++ M04-use-intelligent-planners/M04-Project/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.SemanticKernel;
+
+string yourDeploymentName = "";
+string yourEndpoint = "";
+string yourKey = "";
+
+var builder = Kernel.CreateBuilder();
+builder.Services.AddAzureOpenAIChatCompletion(
+    yourDeploymentName,
+    yourEndpoint,
+    yourKey,
+    "gpt-35-turbo-16k");
+var kernel = builder.Build();
+
+kernel.ImportPluginFromType<MusicLibraryPlugin>();
+
+string prompt = @"This is a list of music available to the user:
+    {{MusicLibraryPlugin.GetMusicLibrary}} 
+
+    This is a list of music the user has recently played:
+    {{MusicLibraryPlugin.GetRecentPlays}}
+
+    Based on their recently played music, suggest a song from
+    the list to play next";
+
+var result = await kernel.InvokePromptAsync(prompt);
+Console.WriteLine(result);
