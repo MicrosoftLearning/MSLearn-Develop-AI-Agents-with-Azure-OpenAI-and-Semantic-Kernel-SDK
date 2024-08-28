a49cfef8a00ce52a0c5274fc7f3b38aaed50c4d2
Becky Zahid <buzahid@microsoft.com>
2024-08-28 10:20:47 -0700

    Added suggest concert plugin

--- M05-use-intelligent-planners/M05-Project/Program.cs
+++ M05-use-intelligent-planners/M05-Project/Program.cs
@@ -1,27 +1,36 @@
 ﻿using Microsoft.SemanticKernel;
-
-string yourDeploymentName = "";
-string yourEndpoint = "";
-string yourApiKey = "";
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+#pragma warning disable SKEXP0060
 
 var builder = Kernel.CreateBuilder();
 builder.AddAzureOpenAIChatCompletion(
-    yourDeploymentName,
-    yourEndpoint,
-    yourApiKey,
+    "gpt-0613",
+    "https://bz-open-ai.openai.azure.com/",
+    "54974face8de471bb5f78f3a8e87677c",
     "gpt-35-turbo-16k");
 var kernel = builder.Build();
-
 kernel.ImportPluginFromType<MusicLibraryPlugin>();
+kernel.ImportPluginFromType<MusicConcertPlugin>();
+kernel.ImportPluginFromPromptDirectory("Prompts");
 
-string prompt = @"This is a list of music available to the user:
-    {{MusicLibraryPlugin.GetMusicLibrary}} 
+OpenAIPromptExecutionSettings settings = new()
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
 
-    This is a list of music the user has recently played:
-    {{MusicLibraryPlugin.GetRecentPlays}}
+var songSuggesterFunction = kernel.CreateFunctionFromPrompt(
+    promptTemplate: @"Based on the user's recently played music:
+        {{$recentlyPlayedSongs}}
+        recommend a song to the user from the music library:
+        {{$musicLibrary}}",
+    functionName: "SuggestSong",
+    description: "Recommend a song from the library"
+);
 
-    Based on their recently played music, suggest a song from
-    the list to play next";
+kernel.Plugins.AddFromFunctions("SuggestSong", [songSuggesterFunction]);
 
-var result = await kernel.InvokePromptAsync(prompt);
-Console.WriteLine(result);
+string prompt = @"Add this song to the recently played songs list:  title: 'Touch', artist: 'Cat's Eye', genre: 'Pop'";
+
+    var result = await kernel.InvokePromptAsync(prompt, new(settings));
+
+    Console.WriteLine(result);
