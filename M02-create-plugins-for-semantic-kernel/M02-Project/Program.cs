6a322d11dc6eb3f395fe1b29a64b340aa6febaa3
Ben Thomas <bentho@microsoft.com>
2024-08-28 11:25:21 -0700

    Configuring projects to use appSettings and user-secrets.

--- M02-create-plugins-for-semantic-kernel/M02-Project/Program.cs
+++ M02-create-plugins-for-semantic-kernel/M02-Project/Program.cs
@@ -1,9 +1,16 @@
 ﻿#pragma warning disable SKEXP0050 
+using Microsoft.Extensions.Configuration;
 using Microsoft.SemanticKernel;
 
-string yourDeploymentName = "";
-string yourEndpoint = "";
-string yourApiKey = "";
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json", optional: true)
+    .AddUserSecrets<Program>(optional: true)
+    .Build();
+
+// Set your values in appsettings.json or user secrets
+string yourDeploymentName = config.GetRequiredSection("yourDeploymentName").Value!;
+string yourEndpoint = config.GetRequiredSection("yourEndpoint").Value!;
+string yourApiKey = config.GetRequiredSection("yourApiKey").Value!;
 
 var builder = Kernel.CreateBuilder();
 builder.AddAzureOpenAIChatCompletion(
