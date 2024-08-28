f2a01b6821a3758255d8fc653568212118600382
Becky Zahid <buzahid@microsoft.com>
2024-08-28 13:04:03 -0700

    Not sure how this happened

--- M05-use-intelligent-planners/M05-Project/Program.cs
+++ M05-use-intelligent-planners/M05-Project/Program.cs
@@ -1,36 +1,15 @@
 ﻿using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
-#pragma warning disable SKEXP0060
+
+string yourDeploymentName = "";
+string yourEndpoint = "";
+string yourApiKey = "";
 
 var builder = Kernel.CreateBuilder();
 builder.AddAzureOpenAIChatCompletion(
-    "gpt-0613",
-    "https://bz-open-ai.openai.azure.com/",
-    "54974face8de471bb5f78f3a8e87677c",
+    yourDeploymentName,
+    yourEndpoint,
+    yourApiKey,
     "gpt-35-turbo-16k");
 var kernel = builder.Build();
-kernel.ImportPluginFromType<MusicLibraryPlugin>();
-kernel.ImportPluginFromType<MusicConcertPlugin>();
-kernel.ImportPluginFromPromptDirectory("Prompts");
 
-OpenAIPromptExecutionSettings settings = new()
-{
-    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
-};
-
-var songSuggesterFunction = kernel.CreateFunctionFromPrompt(
-    promptTemplate: @"Based on the user's recently played music:
-        {{$recentlyPlayedSongs}}
-        recommend a song to the user from the music library:
-        {{$musicLibrary}}",
-    functionName: "SuggestSong",
-    description: "Recommend a song from the library"
-);
-
-kernel.Plugins.AddFromFunctions("SuggestSong", [songSuggesterFunction]);
-
-string prompt = @"Add this song to the recently played songs list:  title: 'Touch', artist: 'Cat's Eye', genre: 'Pop'";
-
-    var result = await kernel.InvokePromptAsync(prompt, new(settings));
-
-    Console.WriteLine(result);
